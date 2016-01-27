b5a7a8914fdf9f06a46846abb97d2f82b88e2d42
bloodyraven <bloody.duj@gmail.com>
2016-01-27 16:17:13 +0100

    init perso

---
+++ src/bean/Personnage.java
@@ -0,0 +1,25 @@
+package bean;
+
+import objet.Anneau;
+import objet.Arme;
+import objet.Botte;
+import objet.Chapeau;
+import objet.Plastron;
+
+public class Personnage {
+
+	private String nom;
+	private int vie;
+	private int mana;
+	private int esq;
+	private int cc; 
+	private int force;
+	private int intel;
+	private Chapeau chapeau;
+	private Plastron plastron;
+	private Anneau anneau1;
+	private Anneau anneau2;
+	private Botte botte;
+	private Arme arme;
+	
+}
---
+++ src/ecran/AbstractPanel.java
@@ -0,0 +1,5 @@
+package ecran;
+
+public class AbstractPanel {
+
+}
